2ffd191e098e7ce72af729ff61d2c146aa0ab263
CharlieHuo570 <charliecr757@gmail.com>
2025-12-19 00:12:04 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🤓Charlie's cool website™")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "I'm super rich."
 )
